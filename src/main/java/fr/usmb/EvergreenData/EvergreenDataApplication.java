2051cfd156e9ff19a30707dbfdbc2011d5087961
Shinyx42 <93926318+Shinyx42@users.noreply.github.com>
2024-04-12 14:02:04 +0200

    test utilisation thymleaf

--- src/main/java/fr/usmb/EvergreenData/EvergreenDataApplication.java
+++ src/main/java/fr/usmb/EvergreenData/EvergreenDataApplication.java
@@ -2,6 +2,7 @@ package fr.usmb.EvergreenData;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,9 @@ public class EvergreenDataApplication {
       SpringApplication.run(EvergreenDataApplication.class, args);
     }
     @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
+    public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    	model.addAttribute("name", name);
+    	return "hello";
+    	//return String.format("Hello %s!", name);
     }
 }
