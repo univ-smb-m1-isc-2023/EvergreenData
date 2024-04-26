0137c8560730fd3d3563c01e5d00beb7abb4afef
Shinyx42 <93926318+Shinyx42@users.noreply.github.com>
2024-04-26 13:17:01 +0200

    ajout formulaire question

--- src/main/java/fr/usmb/EvergreenData/EvergreenDataApplication.java
+++ src/main/java/fr/usmb/EvergreenData/EvergreenDataApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
@@ -19,5 +20,17 @@ public class EvergreenDataApplication {
     	System.out.println(name);
     	return "hello";
     	//return String.format("Hello %s!", name);
+    }
+    
+    @GetMapping("/envoiQuestion")
+    public String envoiQuestion() {
+    	return "formEnvoiQuestion";
+    }
+    
+    @PostMapping(value = "/addQuestion", params = {"question"})
+    public String addQuestion(@RequestParam(name="question", required=true, defaultValue="?") String question, Model model) {
+    	model.addAttribute("question", question);
+    	System.out.println(question);
+    	return "redirect:/envoiQuestion";
     }
 }
